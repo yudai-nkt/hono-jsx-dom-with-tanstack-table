88117d3e5c0f9984875f8d9efe1f298a622e1282
Yudai NAKATA <yudai.nkt@gmail.com>
2024-04-27 01:59:01 +0900

    build: set base path so that GitHub Pages can handle paths to assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import nitrogql from "@nitrogql/rollup-plugin";
 import { type UserConfig } from "vite";
 
 export default {
+  base: "/hono-jsx-dom-with-tanstack-table/",
   resolve: { alias: { react: "hono/jsx/dom", "react-dom": "hono/jsx/dom" } },
   plugins: [nitrogql({ include: ["**/*.graphql"] })],
 } satisfies UserConfig;
