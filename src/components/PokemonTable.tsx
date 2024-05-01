23ed61c894275338762eefb9c472c7be558d0c60
Yudai NAKATA <yudai.nkt@gmail.com>
2024-05-02 04:46:50 +0900

    fix: avoid infinite rendering loop
    cf.: https://tanstack.com/table/latest/docs/faq#pitfall-1-creating-new-columns-or-data-on-every-render

--- src/components/PokemonTable.tsx
+++ src/components/PokemonTable.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "hono/jsx";
 import { useQuery } from "urql";
 import {
   createColumnHelper,
@@ -12,44 +13,59 @@ import { Pagination } from "./Pagination";
 import { SortIndicator } from "./SortIndicator";
 import Query from "../graphql/pokemon.graphql";
 
+type Pokemon = {
+  id: number;
+  name: string;
+  type1: string;
+  type2: string;
+  gen: number | null | undefined;
+};
+
 export const PokemonTable = () => {
   const [{ fetching, data }] = useQuery({ query: Query });
 
-  if (fetching) {
-    return <p>Loading...</p>;
-  }
+  return fetching || !data ? (
+    <p>Loading...</p>
+  ) : (
+    <Table
+      data={data.pokemon.map((p) => ({
+        id: p.id,
+        name: p.name,
+        type1: p.types[0]?.pokemon_v2_type?.name ?? "N/A",
+        type2: p.types[1]?.pokemon_v2_type?.name ?? "N/A",
+        gen: p.specy?.generation_id,
+      }))}
+    />
+  );
+};
 
-  const pokemon = data!.pokemon.map((p) => ({
-    id: p.id,
-    name: p.name,
-    type1: p.types[0]?.pokemon_v2_type?.name ?? "N/A",
-    type2: p.types[1]?.pokemon_v2_type?.name ?? "N/A",
-    gen: p.specy?.generation_id,
-  }));
-
-  const columnHelper = createColumnHelper<(typeof pokemon)[number]>();
-  const columns = [
-    columnHelper.accessor("id", {
-      header: ({ column }) => (
-        <span>
-          Pokédex #{" "}
-          <button onClick={column.getToggleSortingHandler()}>
-            <SortIndicator direction={column.getIsSorted()} />
-          </button>
-        </span>
-      ),
-    }),
-    columnHelper.accessor("name", { header: "Name" }),
-    columnHelper.accessor("type1", { header: "Type 1" }),
-    columnHelper.accessor("type2", { header: "Type 2" }),
-    columnHelper.accessor("gen", {
-      header: "Debut in",
-      cell: ({ row }) => `Gen ${row.original.gen}`,
-    }),
-  ];
+const Table = ({ data }: { data: Pokemon[] }) => {
+  const columnHelper = createColumnHelper<Pokemon>();
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor("id", {
+        header: ({ column }) => (
+          <span>
+            Pokédex #{" "}
+            <button onClick={column.getToggleSortingHandler()}>
+              <SortIndicator direction={column.getIsSorted()} />
+            </button>
+          </span>
+        ),
+      }),
+      columnHelper.accessor("name", { header: "Name" }),
+      columnHelper.accessor("type1", { header: "Type 1" }),
+      columnHelper.accessor("type2", { header: "Type 2" }),
+      columnHelper.accessor("gen", {
+        header: "Debut in",
+        cell: ({ row }) => `Gen ${row.original.gen}`,
+      }),
+    ],
+    []
+  );
 
   const table = useReactTable({
-    data: pokemon,
+    data,
     columns,
     initialState: {
       pagination: { pageSize: 200 },
@@ -60,10 +76,9 @@ export const PokemonTable = () => {
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
   });
-
   return (
     <>
-      <label for="gen-filter">Filter by generation:</label>
+      <label htmlFor="gen-filter">Filter by generation:</label>
       <select
         name="generation"
         id="gen-filter"
@@ -75,7 +90,9 @@ export const PokemonTable = () => {
       >
         <option value="">Choose a generation</option>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((gen) => (
-          <option value={gen}>Gen {gen}</option>
+          <option value={gen} key={gen}>
+            Gen {gen}
+          </option>
         ))}
       </select>
       <Pagination table={table} />
